8c5f3d3570d262ea1361e38aaa0d9252d1ff9097
Magnus Glantz <sudo@redhat.com>
2019-04-09 07:58:24 +0200

    Create list.c

---
+++ list.c
@@ -0,0 +1,175 @@
+Ansible server:
+3.121.227.35
+Managed systems:
+18.197.79.34
+3.122.224.187
+52.58.228.203
+
+
+Ansible server:
+18.194.246.171
+Managed systems:
+3.121.42.117
+18.184.94.192
+18.185.137.27
+
+Management node:
+54.93.43.166
+Managed systems:
+18.197.40.182
+3.121.29.186
+3.121.116.15
+
+Management node:
+52.29.131.228
+Managed systems:
+54.93.53.22
+52.59.207.249
+52.59.230.202
+
+Ansible server:
+18.185.131.252
+Managed systems:
+3.122.230.62
+18.184.233.163
+18.185.18.205
+
+Ansible server:
+18.197.104.142
+Managed systems:
+35.157.155.218
+54.93.173.157
+35.158.139.117
+
+Ansible server:
+54.93.111.203
+Managed systems:
+18.195.113.85
+3.120.38.68
+18.195.51.107
+
+Ansible server:
+18.197.122.113
+Managed systems:
+18.184.186.223
+52.59.114.207
+54.93.46.83
+
+Ansible server:
+54.93.111.75
+Managed systems:
+52.57.16.125
+18.185.138.82
+35.158.118.98
+
+Ansible server:
+52.59.228.199
+Managed systems:
+54.93.180.34
+35.157.43.242
+18.185.136.44
+
+Ansible server:
+18.184.22.102
+Managed systems:
+18.185.83.244
+54.93.75.145
+54.93.162.36
+
+Ansible server:
+3.122.114.215
+Managed systems:
+3.121.200.146
+3.122.223.208
+18.195.242.213
+
+Ansible server:
+52.59.250.110
+Managed systems:
+18.184.3.203
+3.121.200.28
+3.121.42.238
+
+Ansible server:
+18.194.87.41
+Managed systems:
+35.157.220.163
+3.121.233.200
+18.195.55.184
+
+Ansible server:
+18.197.149.142
+Managed systems:
+18.197.147.140
+54.93.180.41
+18.185.117.98
+
+Ansible server:
+54.93.208.240
+Managed systems:
+18.194.42.23
+18.184.144.115
+3.122.234.145
+
+Ansible server:
+18.184.170.31
+Managed systems:
+35.158.2.219
+18.184.151.196
+35.156.254.216
+
+Ansible server:
+54.93.55.2
+Managed systems:
+18.194.103.28
+18.194.217.191
+35.158.239.70
+
+Ansible server:
+3.121.42.64
+Managed systems:
+18.197.26.123
+54.93.202.109
+18.194.163.246
+
+Ansible server:
+54.93.123.250
+Managed systems:
+3.122.224.108
+3.122.192.12
+54.93.63.167
+
+Ansible server:
+3.121.227.56
+Managed systems:
+18.185.96.6
+18.195.88.217
+18.195.113.225
+
+Ansible server:
+18.197.151.162
+Managed systems:
+3.123.2.190
+18.195.35.132
+54.93.86.36
+
+Ansible server:
+54.93.109.119
+Managed systems:
+18.196.1.210
+3.121.200.114
+18.185.132.103
+
+Ansible server:
+52.58.140.64
+Managed systems:
+35.159.33.23
+18.184.154.76
+18.185.127.84
+
+Ansible server:
+18.197.101.29
+Managed systems:
+18.197.149.231
+18.194.231.186
+54.93.254.61
